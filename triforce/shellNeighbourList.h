dded22ea42d7479ce11686bbf7c65bd002d183df
nils <nils@376bb805-d709-4451-8b19-f41d6d19b301>
2013-02-27 15:57:01 +0000

    added a neighbourlist algorithm that is based on overlapping spheres. It uses two layers of administration: One overlapping spherical and one cubical layer. The cubical layer is used to associate cubical patches of space with spheres that intersect them. The spherical layer holds neighbourlists of spheres that intersect them. Inserting an atom is done in O(m) (probably actually O(m*log(n)) depending on implementation), where m is based on the grid spacing. Retrieving the neighbour list for one atom is done in O(n), where n corresponds to the number of neighbours. Removing an atom  involves O(m*log(n)).  Precalculations are necessary for that list which are at the moment very naively implemented and might be expensive (but are one time costs anyways)

---
+++ triforce/shellNeighbourList.h
@@ -0,0 +1,77 @@
+/* Copyright 2013, Nils J. D. Drechsel & Jordi Villà-Freixa
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef SHELL_NEIGHBOUR_LIST_H_
+#define SHELL_NEIGHBOUR_LIST_H_
+
+#include <string>
+#include <vector>
+#include <set>
+#include "boost/multi_array.hpp"
+
+#include <armadillo>
+
+#include "data3d.h"
+#include "molecule.h"
+
+
+using namespace std;
+using namespace arma;
+
+typedef struct
+{
+	int x;
+	int y;
+	int z;
+}
+Coordinate;
+
+typedef boost::multi_array<set<int>,3> ShellGrid;
+typedef boost::multi_array<vector<Coordinate>,3> CubicalGrid;
+typedef boost::multi_array<double,3> CubicalTemporaryGrid;
+
+
+class ShellNeighbourList{
+	
+public:
+	ShellNeighbourList(Vector &center, Vector &dim, double searchRadius, int sphericalDetail, int cubicalDetail);
+	
+	void addSphere(Vector &x, int id);
+	set<int> getNeighbors(Vector &x);
+	void deleteSphere(Vector &x, int id);
+	
+
+private:
+	Vector dim;
+	Vector center;
+	VectorInt numSpheres;
+	VectorInt numCubes;
+	Vector origin;
+	double shellRadius;
+	double cubicalLength;
+	double sphericalDistance;
+	double sphericalDetail;
+	double cubicalDetail;
+	
+	ShellGrid *shellGrid;
+	CubicalGrid *cubicalGrid;
+
+	
+	
+};
+
+#endif //SHELL_NEIGHBOUR_LIST_H_
